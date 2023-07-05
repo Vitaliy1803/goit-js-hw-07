7c16e2b22bf065503f54a504f4ae200911f44e43
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-05 21:47:44 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,4 +3,40 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryContainer = document.querySelector('.gallery');
 
+const itemsGallery = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+
+
+    galleryContainer.insertAdjacentHTML('beforeend', itemsGallery);
+
+    galleryContainer.addEventListener('click', onImgClick);
+
+
+function onImgClick (event) {
+    event.preventDefault();
+    console.log(event.target.nodeName);
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+ });
